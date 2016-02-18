40da3c78bcb08d6d252936a28c2a967b3ae8f190
Michael Rouse <mwrouse@outlook.com>
2016-02-18 13:36:17 -0600

    New Player Class

--- js/player.class.js
+++ js/player.class.js
@@ -1,49 +1,41 @@
-require_once('sprite.class');
-
-
-
-var HIGH_HEALTH = '#088A08';
-var MED_HEALTH = '#C18C08';
-var LOW_HEALTH = '#DC2417';
-
-var IDLE = 0;
-var WALKING_DOWN = 1;
-
-var Directions = { left: 0, right: 1, up: 2, down: 3, idle: 3};
-
-
 /*
  * ===============================
  *        Player Object
  * ===============================
  */
+require_once('sprite.class');
+
+var Directions = {left: 0, right:1, up:2, down:3, idle:4};
+/*
+ * Player Class Object
+ */
 function PlayerObj()
 {
-  // Player Stats
-  var health = 100;
+  /*
+   * Private variables
+   */
+  var _health = 100;     // Player's Health
 
-	// Player coordinates and movement
-	this.location = new Coordinates();
+  var _location = new Coordinates(); // Player's coordinates on the screen (pixels)
 
-	this.steps = 8; // Number of pixels to move per step
+  var _width = 48; // Width of sprites (pixels)
+  var _height = 66; // Height of sprites (pixels)
 
-  // Player dimensions
-  this.width = 48;
-  this.height = 66;
+  var _moving = false; // True when character walking animations should show
 
-  // True if player is walking
-  this.walking = false;
+  var _attacking = false; // True when character attacking animations should show
 
-  // Player orientation and animation
-  this.direction = Directions.down;
+  var _direction = null; // Down is the starting direction
 
+  var _speed = 8; // Character's speed (pixels)
 
-
-  // True if the player is attacking
-  this.attacking = false;
+  var _sprite = null; // Current sprite to show for the character
 
 
-  // All the sprites for the player
+
+  /*
+   * Public Variables
+   */
   this.sprites = {
     // Sprites for left
     left: {
@@ -119,114 +111,218 @@ function PlayerObj()
     }
   };
 
+  this.animationFrame = 0; // Current frame of an animation to show
 
-  // Animation controls
-  this.animation = {
-    direction: this.sprites.down, // Direction to pull animation stuff from
-    frame: 0                      // Current animation frame
+
+
+
+
+  /*
+   * Privileged Functions (Can access private variables)
+   */
+
+  // Accessors for private variables
+  this.location = function() { return _location; };
+  this.width = function() { return _width; };
+  this.height = function() { return _height; };
+  this.isMoving = function() { return _moving; };
+  this.startMoving = function() { _moving = true; return true; };
+  this.stopMoving = function() { _moving = false; return false; };
+  this.isAttacking = function() { return _attacking; };
+  this.attack = function()
+  {
+    this.animationFrame = (!_attacking) ? 1 : this.animationFrame;
+    _attacking = true;
+    return true;
+  };
+  this.direction = function() { return _direction; };
+  this.speed = function() { return _speed; };
+  this.sprite = function() { return _sprite; };
+
+  // Accessor/mutator for palyer health
+  this.health = function(hp_increase)
+  {
+    if (hp_increase !== undefined)
+    {
+      _health += hp_increase;
+      if (_health > 100) { _health = 100; }
+      this.updateHealth();
+    }
+
+    return _health;
   };
 
 
-	// Player sprite
-	this.sprite = this.animation.direction.idle;
+  /*
+   * Do damage to the character
+   */
+  this.damage = function(amount)
+  {
+    // Make sure amount is negative
+    if (amount > 0) { amount *= -1; }
+    return this.health(amount);
+  };
 
 
-  // Health Object
-  this.health_lbl = $('#health_val');
+  /*
+   * Returns Coordinates of different locations on the character
+   */
+  this.rightFoot = this.rightBottom = function()
+  {
+    return new Coordinates(this.location().x + this.width(), this.location().y + this.height());
+  };
+  this.leftFoot = this.leftBottom = function()
+  {
+    return new Coordinates(this.location().x, this.location().y + this.height());
+  };
+  this.centerFoot = this.centerBottom = function()
+  {
+    return new Coordinates(this.location().x + Math.round(this.width() / 2), this.location().y + this.height());
+  };
+  this.centerFootTop = this.center = function()
+  {
+    return new Coordinates(this.location().x + Math.round(this.width() / 2), this.location().y + Math.round(this.height() / 2));
+  };
 
 
-
-  /* ==========================
-       Privileged Methods
-     ========================== */
-  this.health = function(){ return health; }
-
-  // Do damage to the character
-  this.damage = function(amount){
-    health -= amount;
-
-    // Update the health label
-    this.updateHealth();
-
-    return health;
-  }
-
-
-  // ---------------------------------
-  // Updates the sprite for animations
-  // ---------------------------------
-  this.updateSprite = function(){
-    // ----------------------------
-    //  Advance and show Animation
-    // ----------------------------
-    if (this.attacking)
+  /*
+   * Updates the character's sprite for animation
+   */
+  this.updateSprite = function ()
+  {
+    // --------- Attacking Animation ---------
+    if (this.isAttacking())
     {
-      // -------- Attacking Animation --------
-      this.attacking = (this.animation.frame != 0);
+      _attacking = (this.animationFrame != 0); // Update if the character is attacking
 
-      switch (this.animation.frame)
+      switch (this.animationFrame)
       {
         case 0:
         case 1:
-          this.sprite = this.animation.direction.idle;
+          _sprite = this.direction().idle;
           break;
 
         case 2:
         case 3:
-          this.sprite = this.animation.direction.attacking.frame1;
+          _sprite = this.direction().attacking.frame1;
           break;
 
         case 4:
         case 5:
-          this.sprite = this.animation.direction.attacking.frame2;
+          _sprite = this.direction().attacking.frame2;
           break;
       }
 
-      // Advance to the next frame
-      this.animation.frame++;
-      if (this.animation.frame > 5) { this.animation.frame = 0; }
+      // Advance to the next frame, or loop back to frame 0
+      this.animationFrame += (this.animationFrame >= 5) ? (this.animationFrame * -1) : 1;
+
     }
 
-    else if (this.moving)
+    // --------- Walking Animation ---------
+    else if (this.isMoving())
     {
-      // -------- Walking Animation --------
-      this.sprite = (this.animation.frame == 0) ? this.animation.direction.walking.frame1 : this.animation.direction.walking.frame2;
-      this.animation.frame = (this.animation.frame == 0) ? 1 : 0; // Advance to the next frame on next draw
+      _sprite = (this.animationFrame == 0) ? this.direction().walking.frame1 : this.direction().walking.frame2;
+
+      this.animationFrame = (this.animationFrame == 0) ? 1 : 0; // Advance to next frame or loop
     }
 
+    // --------- Idle ---------
     else
     {
-      // -------- Idle --------
-      this.sprite = this.animation.direction.idle;
-      this.animation.frame = 0;
+      _sprite = this.direction().idle;
+      this.animationFrame = 0;
     }
   };
 
 
-  // Returns Coordinates of diferent areas on the character
-  this.rightFoot = function(){
-    return new Coordinates(this.location.x + this.width, this.location.y + this.height);
-  }
-  this.leftFoot = function(){
-    return new Coordinates(this.location.x, this.location.y + this.height);
-  }
-  this.centerFoot = function(){
-    return new Coordinates(this.location.x + Math.round(this.width / 2), this.location.y + this.height);
-  }
-  this.centerFootTop = this.center = function(){
-    return new Coordinates(this.location.x + Math.round(this.width / 2), this.location.y + Math.round(this.height / 2));
-  }
+  /*
+   * Move the character on the x axis
+   */
+  this.moveX = function(direction)
+  {
+    var speed = direction * this.speed();
+
+    var foots = (direction == -1) ? this.leftFoot() : this.rightFoot();
+
+    var condition = (direction == -1) ? (this.location().x <= (screen.width * 0.40)) : (this.location().x >= (screen.width * 0.40));
+
+    // Make sure the next tile is valid
+    _moving = map.validMove(foots.x + speed, foots.y);
+
+    // Change the direction
+    _direction = (direction == -1) ? this.sprites.left : this.sprites.right;
+
+    if (this.isMoving())
+    {
+      // Move the map if possible
+      if (condition)
+      {
+        var shifted = (direction == -1) ? map.shiftLeft(foots.x + speed, foots.y) : map.shiftRight(foots.x + speed, foots.y);
+
+        if (shifted)
+        {
+          return;
+        }
+      }
+
+      // Move character towards screen edge
+      _location.x += speed;
+    }
+  };
+
+  /*
+   * Move character on the y axis
+   */
+  this.moveY = function(direction)
+  {
+    var speed = direction * this.speed();
+
+    var side = (direction == -1) ? this.centerFootTop() : this.centerFoot();
+
+    var condition = (direction == -1) ? (this.location().y <= (screen.height * 0.40)) : (this.location().y >= (screen.height * 0.40));
+
+    // Make sure the next tile is valid
+    _moving = map.validMove(side.x, side.y + speed);
+
+    // Change the direction
+    _direction = (direction == -1) ? this.sprites.up : this.sprites.down;
+
+    if (this.isMoving())
+    {
+      // Move the map if possible
+      if (condition)
+      {
+        var shifted = (direction == -1) ? map.shiftUp(side.x, side.y + speed) : map.shiftDown(side.x, side.y + speed);
+
+        if (shifted)
+        {
+          return;
+        }
+      }
+      // Move the character towards a screen edge
+      _location.y += speed;
+    }
+  };
 
 
 
 
-  // This will make the health display on load (constructor), from then on out it will only be updated when the player gets damage done to them
+
+
+
+  /*
+   * Constructor stuff
+   */
+  _direction = this.sprites.down;
+  _sprite = _direction.idle;
+
   this.updateHealth();
-
-
-
-
 }
+
+
+
+
+
 
 
 
@@ -238,171 +334,85 @@ function PlayerObj()
  * ===============================
  */
 
-/* ----------------
- *  Draw on Buffer
- * ---------------- */
-PlayerObj.prototype.draw = function(){
+
+
+/*
+ * Draw on buffer
+ */
+ PlayerObj.prototype.draw = function()
+ {
   // Update the sprite
   this.updateSprite();
-  this.sprite.draw(this.location.x, this.location.y);
+  this.sprite().draw(this.location().x, this.location().y);
 
   // Reset
-  this.moving = false;
+  this.stopMoving();
+ }
+
+
+
+ /*
+  * Move Left
+  */
+PlayerObj.prototype.moveLeft = function()
+{
+  return this.moveX(-1);
+}
+
+/*
+ * Move Right
+ */
+PlayerObj.prototype.moveRight = function()
+{
+  return this.moveX(1);
+}
+
+/*
+ * Move Up
+ */
+PlayerObj.prototype.moveUp = function()
+{
+  return this.moveY(-1);
 };
 
-
-
-/* ----------------
- *    Move Left
- * ---------------- */
-PlayerObj.prototype.moveLeft = function() {
-  // Make sure the player has not moved off the screen and is a valid tile
-  this.moving = (map.validMove(this.leftFoot().x - this.steps, this.leftFoot().y));
-
-  // Make left the new direction
-  this.direction = Directions.left;
-  this.animation.direction = this.sprites.left;
-
-  // Move map if possible
-  if (this.location.x <= (screen.width * 0.40))
-  {
-    if (map.shiftLeft(this.leftFoot().x - this.steps, this.leftFoot().y))
-    {
-      return;
-    }
-  }
-
-  // Move the character if the map could not shift
-  if (this.moving)
-  {
-    this.location.x -= this.steps;
-  }
-};
-
-/* ----------------
- *    Move Right
- * ---------------- */
-PlayerObj.prototype.moveRight = function() {
-  // Make sure that the location does not fall on a restricted tile
-  this.moving = map.validMove(this.rightFoot().x + this.steps, this.rightFoot().y);
-
-  // Make the character facing right
-  this.direction = Directions.right;
-  this.animation.direction = this.sprites.right;
-
-  // Move the map if possible
-  if ( this.location.x >= (screen.width - (screen.width * 0.40)) )
-  {
-    if (map.shiftRight(this.rightFoot().x , this.rightFoot().y))
-    {
-      return; // Map shifted, return (Note, that if the map senses the character will be in a restricted tile, it will still return true)
-    }
-  }
-
-  // Move the character if the map did not shift
-  if (this.moving)
-  {
-    this.location.x += this.steps;
-  }
-};
-
-/* ----------------
- *    Move Up
- * ---------------- */
-PlayerObj.prototype.moveUp = function() {
-  // Make sure the player will be on the screen
-  this.moving = (map.validMove(this.centerFootTop().x, this.centerFootTop().y - this.steps));
-
-  // Character is going up
-  this.direction = Directions.up;
-  this.animation.direction = this.sprites.up;
-
-  // Only move the map if the character is not out of range or stuff
-  if (this.location.y <= (screen.height * 0.40))
-  {
-    if (map.shiftUp(this.centerFootTop().x, this.centerFootTop().y - this.steps))
-    { return; }
-  }
-
-
-  // Map was not able to shift, move the player if possible
-  if (this.moving)
-  {
-    this.location.y -= this.steps;
-  }
-
-};
-
-/* ----------------
- *    Move Down
- * ---------------- */
-PlayerObj.prototype.moveDown = function() {
-  // Make sure the player will be on the screen
-  this.moving = (map.validMove(this.centerFoot().x, this.centerFoot().y + this.steps));
-
-  // Character is going down
-  this.direction = Directions.down;
-  this.animation.direction = this.sprites.down;
-
-  // Only move the map if the character is in a range or something like that.
-  if (this.location.y >= (screen.height - (screen.height * .40)))
-  {
-
-    if (map.shiftDown(this.centerFoot().x, this.centerFoot().y + this.steps))
-    {
-      this.moving = true;
-      return;
-    }
-  }
-
-  // Map was not able to shift, move the character
-  if (this.moving)
-  {
-    this.location.y += this.steps;
-    this.moving = true;
-  }
-};
-
-
-/* ----------------
- *     Attack
- * ---------------- */
-PlayerObj.prototype.attack = function(){
-  if (!this.attacking)
-  {
-    this.animation.frame = 1; // Set animation number to zero
-  }
-
-  // Set the character as attacking
-  this.attacking = true;
+/*
+ * Move Down
+ */
+PlayerObj.prototype.moveDown = function()
+{
+  return this.moveY(1);
 };
 
 
 
 
 
-/* ----------------
- *  Set Health Value
- * ---------------- */
-PlayerObj.prototype.updateHealth = function(){
-  // Update the text label
-  this.health_lbl.html(this.health());
+/*
+ * Update health on the screen
+ */
+PlayerObj.prototype.updateHealth = function()
+{
+  var lbl = $('#health_val');
 
-  // update the color of the text to represent the level of health
-  this.health_lbl.css('color', (this.health() > 70) ? HIGH_HEALTH : (this.health() > 40) ? MED_HEALTH : LOW_HEALTH);
+  lbl.html(this.health()); // Update the label
+  lbl.css('color', (this.health() > 70) ? '#088A08' : (this.health() > 40) ? '#C18C08' : '#DC2417');
 };
 
 
-
-
-/* ----------------
- *   Pickup Items
- * ---------------- */
+/*
+ * Pickup Items
+ */
 PlayerObj.prototype.pickup = function()
 {
-  if (map.pickupItem(this.center().x, this.center().y) != null)
+  var item = map.pickupItem(this.center().x, this.center().y);
+
+  if (item != null)
   {
-    console.log('item picked up!');
+    // Increase health if the item was a health pack
+    if (item == HEALTH_PACK)
+    {
+      this.health(rand(rand(10, 50), rand(50, 70))); // Random health between a random low number a random high number
+    }
   }
 };
 
@@ -418,9 +428,16 @@ PlayerObj.prototype.pickup = function()
 
 
 
+
+
+
+
+
+
+
+
+// The player object :)
 var player = new PlayerObj();
-
-
 
 
 
